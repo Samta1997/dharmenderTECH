f8fc575c184e7c2d37e234f7574b0dd67889b865
Samta1997 <dharmendersinghsml@gmail.com>
2024-01-29 16:26:57 +0530

    added ThemeProvider, GlobalStyle and Header styling using styled component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,28 @@
 import { NavLink } from "react-router-dom"
 import Navbar from "./Navbar"
+import styled from "styled-components"
 
 const Header = () => {
   return (
-    <header>
+    <MainHeader>
       <NavLink to="/">
-        <img src="./images/logo.png" alt="logo"/>
+        <img src="./images/logo.png" alt="logo" className="logo"/>
       </NavLink>
       <Navbar/>
-    </header>
+    </MainHeader>
   )
 }
+const MainHeader=styled.header`
+  padding:0 4.5rem;
+  height:8rem;
+  display:flex;
+  justify-content:space-between;
+  background-color:${({theme})=>theme.colors.bg};
+  .logo{
+    height:3rem;
+  }
+  
+  
+`
 
 export default Header
